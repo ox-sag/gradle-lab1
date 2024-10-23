7046e46fdefbfe383de122d7bc82d4b91014805d
Your Name <sagarprasad@Sagars-MacBook-Air.local>
2024-10-23 17:45:49 +0530

    chore: code update

--- app/src/test/java/org/example/AppTest.java
+++ app/src/test/java/org/example/AppTest.java
@@ -4,11 +4,36 @@
 package org.example;
 
 import org.junit.jupiter.api.Test;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
     @Test void appHasAGreeting() {
         App classUnderTest = new App();
         assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+    }
+    @Test void testLongTask() throws IOException {
+        int timeTaken = performTaskAndRecordTime();
+
+        // Record the time taken
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource("data.txt").getFile());
+        String outputFilePath = file.getAbsolutePath().replace("data", "timing") ;
+        new File(outputFilePath).getParentFile().mkdirs();
+        BufferedWriter writer = new BufferedWriter(new BufferedWriter(
+                new java.io.FileWriter(outputFilePath.toString())));
+        writer.write("Time taken: " + timeTaken +" seconds") ;
+        writer.close();
+
+    }
+
+    private int performTaskAndRecordTime() {
+        long start = System.currentTimeMillis();
+        long end = System.currentTimeMillis();
+        return (int) (end - start);
     }
 }
